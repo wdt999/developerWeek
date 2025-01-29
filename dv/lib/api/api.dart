1a405e1cc0de3f3a25e331a341eae31823e34138
junyusun <wdt999@sookmyung.ac.kr>
2025-01-29 22:37:19 +0900

    👽️ API 연결 설정정

---
+++ dv/lib/api/api.dart
@@ -0,0 +1,5 @@
+//api.dart
+
+class API {
+  static const hostConnect = "http://192.168.80.1/developerWeek";
+}
---
+++ dv/lib/api/connection.php
@@ -0,0 +1,10 @@
+//connection.php
+
+<?php
+$myServer = "localhost";
+$user = "root";
+$password = "";
+$database = "talking_potato";
+
+//데이터베이스 연결
+$connection = new mysqli($id, $name, $password);
