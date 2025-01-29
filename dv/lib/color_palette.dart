83973969e3b201bf5dcc88313525e5048be68209
Eunjin <eunjinlee0128@gmail.com>
2025-01-29 19:33:39 +0900

    ✨ 색상 팔레트 추가
    각 테마별 색상 팔레트 추가

---
+++ dv/lib/color_palette.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  // 식물 테마에 사용할 색상 팔레트
+  List<Color> botanic = [
+    Color(0x00123524),
+    Color(0x003e7b27),
+    Color(0x0085a947),
+    Color(0x00efe3c2)
+  ];
+
+  // 식기 테마에 사용할 색상 팔레트
+  List<Color> cutlery = [
+    Color(0x00d9dfc6),
+    Color(0x00eff3ea),
+    Color(0x00fffdf0),
+    Color(0x00fff2c2)
+  ];
+
+  // 술 테마에 사용할 색상 팔레트
+  List<Color> alcohol = [
+    Color(0x0009122c),
+    Color(0x00872341),
+    Color(0x00be3144),
+    Color(0x00e17564)
+  ];
+
+  // 원석 테마에 사용할 색상 팔레트
+  List<Color> gemstone = [
+    Color(0x00050c9c),
+    Color(0x003572ef),
+    Color(0x003abef9),
+    Color(0x00a7e6ff)
+  ];
+
+}
